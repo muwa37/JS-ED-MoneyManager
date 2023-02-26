8ff84b89fdc4ff26a529251fea85cef9536cd8ca
muwa37 <yond41m3@icloud.com>
2023-02-26 21:49:38 +0600

    Changed register form

--- public/js/ui/forms/RegisterForm.js
+++ public/js/ui/forms/RegisterForm.js
@@ -1,15 +1,23 @@
-/**
- * Класс RegisterForm управляет формой
- * регистрации
- * */
+/*
+ * Управляет формой регистрации
+*/
 class RegisterForm extends AsyncForm {
-  /**
-   * Производит регистрацию с помощью User.register
-   * После успешной регистрации устанавливает
+  /*
+   * Производит регистрацию, устанавливает
    * состояние App.setState( 'user-logged' )
    * и закрывает окно, в котором находится форма
-   * */
-  onSubmit(data) {
+  */
+  onSubmit(options) {
+    User.register(options.data, (err, response) => {
+      if (!(response.success)) {
+        return;
+      }
+      this.element.reset();
 
+      App.setState('user-logged');
+
+      const modal = new Modal( this.element.closest('.modal'));
+      modal.close().unregisterEvents();
+    });
   }
 }
