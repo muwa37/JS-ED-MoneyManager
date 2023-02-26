ea1398aae693854fc4378f5d8df8bd44d57ceaae
muwa37 <yond41m3@icloud.com>
2023-02-26 21:36:50 +0600

    Changed asyncforms

--- public/js/ui/forms/AsyncForm.js
+++ public/js/ui/forms/AsyncForm.js
@@ -1,49 +1,55 @@
-/**
+/*
  * Класс AsyncForm управляет всеми формами
  * приложения, которые не должны быть отправлены с
  * перезагрузкой страницы. Вместо этого данные
  * с таких форм собираются и передаются в метод onSubmit
  * для последующей обработки
- * */
+*/
 class AsyncForm {
-  /**
-   * Если переданный элемент не существует,
-   * необходимо выкинуть ошибку.
+  /*
    * Сохраняет переданный элемент и регистрирует события
    * через registerEvents()
-   * */
+  */
   constructor(element) {
-
+    if (!(element)) {
+      throw new Error('Elem does not exist');
+    }
+    this.element = element;
+    this.registerEvents();
   }
 
-  /**
-   * Необходимо запретить отправку формы и в момент отправки
-   * вызывает метод submit()
-   * */
+  /*
+   *В момент отправки вызывает метод submit
+  */
   registerEvents() {
-
+    this.element.addEventListener( 'submit', e => {
+      e.preventDefault();
+      this.submit();
+    });
   }
 
-  /**
-   * Преобразует данные формы в объект вида
-   * {
-   *  'название поля формы 1': 'значение поля формы 1',
-   *  'название поля формы 2': 'значение поля формы 2'
-   * }
-   * */
+  /*
+   * Преобразует данные формы в объект
+  */
   getData() {
-
+    return [... ( new FormData( this.element )).entries()]
+        .reduce(( target, [ key, value ]) => {
+          target[ key ] = value;
+          return target;
+        }, {});
   }
 
   onSubmit(options){
 
   }
 
-  /**
-   * Вызывает метод onSubmit и передаёт туда
-   * данные, полученные из метода getData()
-   * */
+  /*
+   * Вызывает метод onSubmit и передаёт туда данные, полученные из getData()
+  */
   submit() {
-
+    const data = this.getData();
+    this.onSubmit({
+      data
+    });
   }
 }
