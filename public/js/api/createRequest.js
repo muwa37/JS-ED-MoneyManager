67c0b27d301cbdc903b3d1c5e705724b35734815
muwa37 <yond41m3@icloud.com>
2023-02-26 17:30:52 +0600

    Added function that create request

--- public/js/api/createRequest.js
+++ public/js/api/createRequest.js
@@ -1,7 +1,23 @@
-/**
- * Основная функция для совершения запросов
- * на сервер.
- * */
-const createRequest = (options = {}) => {
+/* Основная функция для совершения запросов на сервер. */
+"use strict";
+
+const createRequest = (options = {}, callback) => {
+    let url = options.url;
+
+    if (!options.body && !(url === '/user/logout' )) return;
+    
+    if (options.method === 'GET' && (options.body)) {
+        const dataArr = Object.entries(options.body);
+        dataArr.map(([key, value]) => `${key}=${value}`);
+        const encodedData = '?' + dataArr.join('&');
+        url += encodedData;
+        delete options.body;
+        delete options.url;
+    }
+
+    fetch(url, options)
+        .then(response => response.json)
+        .then(data => callback(data))
+        .catch(error => console.log(error));
 
 };
